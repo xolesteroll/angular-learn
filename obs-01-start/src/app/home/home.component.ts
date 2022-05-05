af40e2bced24527ae77674cabc6ed1d73d546b80
Anton <Iarovoi>
2022-05-05 18:41:21 +0300

    observables, Subject (RxJs)

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {interval, Subscription} from "rxjs";
+import {interval, Subscription, Observable} from "rxjs";
+import {filter, map} from "rxjs/operators";
 
 @Component({
   selector: 'app-home',
@@ -12,8 +13,37 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    this.obsSubscription = interval(1000).subscribe(count => {
+    // this.obsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count)
+    // })
+
+    const customObservable = new Observable((observer) => {
+      let count = 0
+      setInterval(() => {
+        observer.next(count)
+        if (count == 2) {
+          observer.complete()
+        }
+        if(count > 3) {
+          observer.error(new Error('COUNT IS GREATER THAN 3!! IMPOSSIBLEEEEE'))
+        }
+        count++
+      }, 1000)
+    })
+
+
+
+    this.obsSubscription = customObservable.pipe(filter((data) => {
+      return data > 0
+    }), map((data: number) => {
+      return `Round: ${data + 1}`;
+    })).subscribe((count) => {
       console.log(count)
+    }, error => {
+      console.log(error)
+      alert(error)
+    }, () => {
+      console.log('completed')
     })
   }
 
