827d37bcf3c89e7b1bd2166e60d2ed7dda6fc758
Пользователь <alaravoi@medialink.com.cy>
2022-07-09 20:16:08 +0300

    changed tslint settings, http(post, get) methods applied

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from "rxjs/operators";
+import {PostModel} from "./post.model";
 
 @Component({
   selector: 'app-root',
@@ -7,22 +9,50 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: PostModel[] = [];
+  isFetching: boolean = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts()
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: PostModel) {
     // Send Http request
-    console.log(postData);
+    this.http.post<{name: string}>('https://angular-http-ea14b-default-rtdb.europe-west1.firebasedatabase.app/posts.json', postData)
+      .subscribe(res => {
+        console.log(res)
+      })
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts()
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.isFetching = true
+    this.http
+      .get<{[key: string]: PostModel}>('https://angular-http-ea14b-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+      .pipe(map((response) => {
+        const postArray: PostModel[] = []
+        for (const key in response) {
+          if (response.hasOwnProperty(key)) {
+            postArray.push({...response[key], id: key})
+          }
+        }
+        return postArray
+      }))
+      .subscribe(posts => {
+        console.log(posts)
+        this.loadedPosts = posts
+        this.isFetching = false
+      })
   }
 }
---
+++ http-01-start/src/app/post.model.ts
@@ -0,0 +1,5 @@
+export interface PostModel {
+  title: string;
+  content: string;
+  id?: string
+}
